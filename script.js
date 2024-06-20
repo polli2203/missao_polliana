31a725f1cee52041982d83f2894a9e7405f3a08a
polli2203 <146850815+polli2203@users.noreply.github.com>
2024-06-20 08:43:53 -0300

    .

--- script.js
+++ script.js
@@ -77,26 +77,6 @@ let atual = 0;
 let perguntaAtual;
 let historiaFinal = "Chegamos a conclusão da importância da aplicação da Inteligência Emocional no dia a dia para saber lidar com certas situações.";
 
-function mostraPergunta() {
-    if (atual >= perguntas.length) {
-        mostraResultado();
-        return;
-    }
-    perguntaAtual = perguntas[atual];
-    caixaPerguntas.textContent = perguntaAtual.enunciado;
-    caixaAlternativas.textContent = "";
-    mostraAlternativas();
-}
-
-function mostraAlternativas(){
-    for(const alternativa of perguntaAtual.alternativas) {
-        const botaoAlternativas = document.createElement("button");
-        botaoAlternativas.textContent = alternativa.texto;
-        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
-        caixaAlternativas.appendChild(botaoAlternativas);
-    }
-}
-
 function mostraResultado() {
     caixaPerguntas.textContent = "Ao fim diso...";
     textoResultado.textContent = historiaFinal;
